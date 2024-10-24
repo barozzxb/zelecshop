65ef43cdc57678220e03fc35626b858fc99e57b1
HaiAnh251 <149868011+HaiAnh251@users.noreply.github.com>
2024-10-24 08:11:38 +0700

    user

---
+++ src/main/java/vn/zelecshop/configs/Test.java
@@ -0,0 +1,35 @@
+package vn.zelecshop.configs;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+public class Test {
+	public static void main(String[] args) {
+
+		EntityManager enma = JPAConfig.getEntityManager();
+
+		EntityTransaction trans = enma.getTransaction();
+
+		try {
+
+			trans.begin();
+			//goi phuong thuc de insert, updatte, delete
+			trans.commit();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+			trans.rollback();
+
+			throw e;
+
+		} finally {
+
+			enma.close();
+
+		}
+
+	}
+
+}
--- src/main/java/vn/zelecshop/entity/Product.java
+++ src/main/java/vn/zelecshop/entity/Product.java
@@ -1,22 +1,18 @@
 package vn.zelecshop.entity;
 
-
 import java.io.Serializable;
-
-
+import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,22 +21,30 @@ import lombok.NoArgsConstructor;
 @Table(name="Product")
 @NamedQuery(name="Product.findAll", query="SELECT p FROM Product p")
 public class Product implements Serializable {
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int productId;
-	private String productName;
-	private int ProductQuantity;
-	private String ProductPrice;
-	@Column(columnDefinition = "nvarchar(MAX)")
-	private String ProductInfo;
-	@Column(columnDefinition = "nvarchar(255)")
-	
-	private String ProductPicture;
-	private String PManufacture;
-	private int PRating;
+    private static final long serialVersionUID = 1L;
 
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_quantity")
+    private int ProductQuantity;
+
+    @Column(name = "product_price")
+    private BigDecimal ProductPrice;
+
+    @Column(name = "product_info", columnDefinition = "text")
+    private String ProductInfo;
+
+    @Column(name = "product_picture", columnDefinition = "varchar(255)")
+    private String ProductPicture;
+
+    @Column(name = "p_manufacture")
+    private String PManufacture;
+
+    @Column(name = "p_rating")
+    private int PRating;
 }
-
-
